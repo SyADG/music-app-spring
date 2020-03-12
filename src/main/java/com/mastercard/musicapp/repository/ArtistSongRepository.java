11437ad7d65c53eacdb6dff953b0551a28db37a8
Syrus <syrusamadeu@hotmail.com>
2020-03-12 10:28:39 -0300

    Added profiles

--- src/main/java/com/mastercard/musicapp/repository/ArtistSongRepository.java
+++ src/main/java/com/mastercard/musicapp/repository/ArtistSongRepository.java
@@ -3,9 +3,11 @@ package com.mastercard.musicapp.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mastercard.musicapp.entity.ArtistSong;
 
+@Repository
 public interface ArtistSongRepository extends JpaRepository<ArtistSong, Long>{
 	public List<ArtistSong> deleteBySongId(Long songId);
 	public List<ArtistSong> findByArtistId(Long artistId);
--- src/main/java/com/mastercard/musicapp/repository/UserRepository.java
+++ src/main/java/com/mastercard/musicapp/repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.mastercard.musicapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.mastercard.musicapp.entity.User;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(String username);
 }
