3455c8f6145100151ace678358f2a1f098fb0a59
Syrus <syrusamadeu@hotmail.com>
2020-03-23 14:28:40 -0300

    add jar config to pom

--- src/main/java/com/mastercard/musicapp/repository/SongRepository.java
+++ src/main/java/com/mastercard/musicapp/repository/SongRepository.java
@@ -1,13 +1,10 @@
 package com.mastercard.musicapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mastercard.musicapp.entity.Song;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long>{
-	//@Query("DELETE FROM Song WHERE ?1 IN(SELECT id FROM Artist.songs)")
-	//public void deleteByIdCascade(Long id);
 }
