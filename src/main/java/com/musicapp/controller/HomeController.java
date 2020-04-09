6b9a77d73186c97c151b3a53634f44fc6254676f
Syrus <syrusamadeu@hotmail.com>
2020-04-09 11:47:02 -0300

    Added HomeController and remove angular app from main

---
+++ src/main/java/com/musicapp/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.musicapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public static String hello() {
+		return "Hello Artists!";
+	}
+	
+}
